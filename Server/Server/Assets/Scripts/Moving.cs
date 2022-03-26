5ba1282d2e4a4d94073545d9abdf456712f4c04f
Veydron <59504534+Veydron@users.noreply.github.com>
2022-03-26 20:50:55 +0100

    MovementNoAuthoritive

--- Server/Server/Assets/Scripts/Moving.cs
+++ Server/Server/Assets/Scripts/Moving.cs
@@ -4,29 +4,30 @@ using UnityEngine;
 using FishNet.Object;
 public class Moving : NetworkBehaviour
 {
-    public float MoveSpeed = 5f;
-    private CharacterController _characterController;
+    public float moveSpeed = 5f;
+    private Vector3 target;
+    private Vector3 position; 
+    
     void Awake()
     {
-        _characterController = GetComponent<CharacterController>();
+        position = gameObject.transform.position;
     }
 
-    // Update is called once per frame
     void Update()
     {
         Move();
     }
 
-    //[Client(RequireOwnership = true)]
     private void Move()
     {
         if (!base.IsOwner)
             return;
 
+        float step = moveSpeed * Time.deltaTime;
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
-        Vector3 offset = new Vector3(horizontal, Physics.gravity.y, vertical) * (MoveSpeed * Time.deltaTime);
-    
-        _characterController.Move(offset);
+        target = new Vector3(transform.position.x + horizontal, 0, transform.position.z + vertical);
+        transform.position = Vector3.MoveTowards(transform.position, target, step);
     }
+
 }
