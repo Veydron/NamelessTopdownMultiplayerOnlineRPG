2fd84fe3419f3a653e96ad2e98ca25c052a797da
Veydron <59504534+Veydron@users.noreply.github.com>
2022-03-27 22:31:33 +0200

    Done for today

--- Server/Server/Assets/Scripts/MapGrid.cs
+++ Server/Server/Assets/Scripts/MapGrid.cs
@@ -1,22 +1,31 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MapGrid
+public class Grid<TGridObject>
 {
     private int width;
     private int height;
     private float cellSize;
-    private int[,] gridArray;
+    private Vector3 originPosition;
+    private TGridObject[,] gridArray;
 
 
-    public MapGrid(int width, int height, float cellSize)
+    public Grid(int width, int height, float cellSize, Vector3 originPosition, Func<Grid<TGridObject>, int, int, TGridObject> createGridObject)
     {
         this.width = width;
         this.height = height;  
         this.cellSize = cellSize;
+        this.originPosition = originPosition;
 
-        gridArray  = new int[width, height];
+        gridArray  = new TGridObject[width, height];
+
+        for (int x = 0; x < gridArray.GetLength(0); x++) {
+            for (int z = 0; z < gridArray.GetLength(1); z++) {
+                gridArray[x, z] = createGridObject(this, x, z);
+            }
+        }
 
         for (int x = 0; x < gridArray.GetLength(0); x++)
         {
@@ -30,9 +39,31 @@ public class MapGrid
         Debug.DrawLine(GetWorldPosition(width, 0), GetWorldPosition(width, height), Color.black, 100f);
     }
 
-    private Vector3 GetWorldPosition(int x, int z)
-    {
-        return new Vector3(x, 0, z) * cellSize;
+    public int GetWidth() {
+        return width;
     }
 
+    public int GetHeight() {
+        return height;
+    }
+
+    public float GetCellSize() {
+        return cellSize;
+    }
+
+    private Vector3 GetWorldPosition(int x, int z){
+        return new Vector3(x, 0, z) * cellSize + originPosition;
+    }
+
+    public void GetXY(Vector3 worldPosition, out int x, out int z) {
+        x = Mathf.FloorToInt((worldPosition - originPosition).x / cellSize);
+        z = Mathf.FloorToInt((worldPosition - originPosition).z / cellSize);
+    }
+    public TGridObject GetGridObject(int x, int z) {
+        if (x >= 0 && z >= 0 && x < width && z < height) {
+            return gridArray[x, z];
+        } else {
+            return default(TGridObject);
+        }
+    }
 }
--- Server/Server/Assets/Scripts/StartEmpty.cs
+++ Server/Server/Assets/Scripts/StartEmpty.cs
@@ -4,17 +4,31 @@ using UnityEngine;
 using FishNet.Object;
 
 
-public class StartEmpty : NetworkBehaviour
+public class StartEmpty : MonoBehaviour
 {
+    private Pathfinding pathfinding;
+
     // Start is called before the first frame update
     void Start()
     {
-        MapGrid grid = new MapGrid(4,2,1);
+        pathfinding = new Pathfinding(10,10);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            List<PathNode> path = pathfinding.FindPath(0,0,6,8);
+            if (path != null)
+            {
+                for (int i=0; i<path.Count - 1; i++)
+                {
+                    
+                    Debug.DrawLine(new Vector3(path[i].x,0, path[i].z) * 1f + Vector3.one * 0.5f, new Vector3(path[i+1].x,0, path[i+1].z) * 1f + Vector3.one * 0.5f, Color.green, 5f);
+
+                }
+            }        
+        }
     }
 }
